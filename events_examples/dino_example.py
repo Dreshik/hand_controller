34e69b64e109ec8b9b71386f62585fddd0ea1e0d
Vlados <allaods97@gmail.com>
2022-11-26 14:41:30 +0700

    examples: убрано лишнее из dino_ex
    Теперь для применения примера достаточно полностью
    скопировать его содержимое в events.py.

--- events_examples/dino_example.py
+++ events_examples/dino_example.py
@@ -1,24 +1,3 @@
-import pyautogui
-
-
-class Event:
-
-    def __init__(self, check_func, action_if_true, action_if_false):
-        self.check_func      = check_func
-        self.action_if_true  = action_if_true
-        self.action_if_false = action_if_false
-
-    def try_do_action(self, hand):
-        if self.check_func(hand):
-            if self.action_if_true:
-                self.action_if_true(hand)
-            return
-
-        if self.action_if_false:
-            self.action_if_false(hand)
-
-events = []
-
 def up_check(hand):
     return hand.joints[4].position.x > hand.joints[3].position.x
 
